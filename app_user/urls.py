705d40170836579520bd19fd37952c227e313496
Nicolas Grandich <nicolasgrandich@hotmail.com>
2022-07-01 20:35:17 -0300

    avatar

--- app_user/urls.py
+++ app_user/urls.py
@@ -6,6 +6,9 @@ urlpatterns = [
 path('login', views.login_request, name='user-login'),
 path('logout', views.logout_request, name='user-logout'),
 path('register', views.register, name='user-register'),
+path('user/update', views.user_update, name='user-update'),
+path('avatar/load', views.avatar_load, name='avatar-load'),
+
 
 
 ]
--- app_user/views.py
+++ app_user/views.py
@@ -5,8 +5,12 @@ from django.contrib.auth import login, logout, authenticate
 from django.shortcuts import redirect
 from django.contrib.auth.forms import AuthenticationForm, UserCreationForm
 from django.contrib.auth.decorators import login_required
-
-
+from app_user.forms import UserRegisterForm, UserEditForm, AvatarForm
+from django.forms.models import model_to_dict
+from app_user.models import Avatar
+from django.contrib import messages
+from django.shortcuts import redirect
+import os
 
 
 def register(request):
@@ -59,3 +63,47 @@ def login_request(request):
 def logout_request(request):
       logout(request)
       return redirect("")
+
+def user_update(request):
+    user = request.user
+    if request.method == 'POST':
+        form = UserEditForm(request.POST)
+        if form.is_valid():
+            informacion = form.cleaned_data
+            user.email = informacion['email']
+            user.password1 = informacion['password1']
+            user.password2 = informacion['password2']
+            user.save()
+
+            return redirect('')
+
+    form= UserEditForm(model_to_dict(user))
+    return render(
+        request=request,
+        context={'form': form},
+        template_name="app_user/user_form.html",
+    )     
+
+def avatar_load(request):
+    if request.method == 'POST':
+        form = AvatarForm(request.POST, request.FILES)
+        if form.is_valid  and len(request.FILES) != 0:
+            image = request.FILES['image']
+            avatars = Avatar.objects.filter(user=request.user.id)
+            if not avatars.exists():
+                avatar = Avatar(user=request.user, image=image)
+            else:
+                avatar = avatars[0]
+                if len(avatar.image) > 0:
+                    os.remove(avatar.image.path)
+                avatar.image = image
+            avatar.save()
+            messages.success(request, "Imagen cargada exitosamente")
+            return redirect('')
+
+    form= AvatarForm()
+    return render(
+        request=request,
+        context={"form": form},
+        template_name="app_user/avatar_form.html",
+    )        
