7363bf53e02438d628f20e2698d0dc57c0d319f8
Nicolas Grandich <nicolasgrandich@hotmail.com>
2022-07-01 21:32:40 -0300

    heroku

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn tp_final.wsgi
--- tp_final/settings.py
+++ tp_final/settings.py
@@ -129,3 +129,12 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+import dj_database_url  
+db_from_env = dj_database_url.config(conn_max_age=500)  
+DATABASES['default'].update(db_from_env)
+
+STATICFILES_DIRS = (  os.path.join(BASE_DIR, 'static'),)
+
+STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
+
