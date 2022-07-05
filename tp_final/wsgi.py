447f7f2bf1dd1d49c9cfe88275d76b51c6636ac6
Nicolas Grandich <nicolasgrandich@hotmail.com>
2022-07-04 21:51:38 -0300

    heroku3

--- tp_final/wsgi.py
+++ tp_final/wsgi.py
@@ -8,6 +8,6 @@ https://docs.djangoproject.com/en/4.0/howto/deployment/wsgi/
 """
 import os  
 from django.core.wsgi import get_wsgi_application  
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "miSitio.settings")  
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "tp_final.settings")  
 application = get_wsgi_application() 
 
